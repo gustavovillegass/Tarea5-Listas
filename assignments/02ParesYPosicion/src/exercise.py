30010ba92e30f4e67445e7776a00a8017fbe3584
gustavovillegass <110926970+gustavovillegass@users.noreply.github.com>
2022-09-27 23:14:05 -0500

    Update exercise.py

--- assignments/02ParesYPosicion/src/exercise.py
+++ assignments/02ParesYPosicion/src/exercise.py
@@ -1,4 +1,14 @@
 def main():
+    cantidad=int(input('Dime la cantidad de valores que hay en la lista, '))
+    lista=[]
+
+    for num in range(cantidad):
+        lista.append(int(input('Dime cada uno de los valores, ')))
+
+    for cont in range(len(lista)):
+        if lista[cont] % 2 == 0:
+            print("pos "+ str(cont) + ", valor "+ str(lista[cont]))
+
 
 if __name__=='__main__':
     main()
