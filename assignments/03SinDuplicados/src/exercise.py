24a82728d0fb1c5c3815491824cc07d4d51295ad
gustavovillegass <110926970+gustavovillegass@users.noreply.github.com>
2022-09-27 22:44:29 -0500

    Update exercise.py

--- assignments/03SinDuplicados/src/exercise.py
+++ assignments/03SinDuplicados/src/exercise.py
@@ -1,5 +1,19 @@
 def main():
     #escribe tu código abajo de esta línea
+    tam=int(input())
+    lista=[]
+    sin_duplicados=[]
+    if tam>0:
+        for n in range(tam):
+            lista.append(input())
+        for elemento in lista:
+            if elemento not in sin_duplicados:
+                sin_duplicados.append(elemento)
+        print(lista)
+        print(sin_duplicados)
+    else:
+        print("Error")
+
 
 if __name__=='__main__':
     main()
